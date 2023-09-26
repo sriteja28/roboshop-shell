19a862f516125afae8218f04fdf3bb59ed2903cd
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-26 09:45:35 +0530

    First Shell script

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,18 @@
+echo -e "\e[33mInstalling Nginx Server\e[0m"
 dnf install nginx -y
 
+echo -e "\e[33mRemoving old app Server\e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[33mDownloading Front end content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[33mExtract frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 ## Copy roboshop.conf file
 
+echo -e "\e[33mStarting Nginx server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
