1750b29883af047c5621638fd39193e8abe7e309
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-26 09:29:17 +0530

    First Shell script

---
+++ frontend.sh
@@ -0,0 +1,13 @@
+dnf install nginx -y
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+## Copy roboshop.conf file
+
+systemctl enable nginx
+systemctl restart nginx
