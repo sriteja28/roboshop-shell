b2c63dbfe7de4d18e2070f660a62deb321c58e8d
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-26 10:46:24 +0530

    First Shell script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[33mRemoving old app Server\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33mDownloading Front end content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 2>/tmp/error.log
 
 echo -e "\e[33mExtract frontend content\e[0m"
 cd /usr/share/nginx/html
