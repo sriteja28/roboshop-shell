e3906e112bbdb456aaaa0ac94f7e585ff3db5fe2
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-27 16:22:11 +0530

    Playbook

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,8 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
+echo -e "\e[33mupdate mongodb listerner\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33mStart MongoDB Server\e[0m"
 # Modify the config file
